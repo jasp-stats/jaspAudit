c01060ef5e988d36a107fe393da20f7a8d12347c
Koen Derks <koen-derks@hotmail.com>
2020-11-27 16:12:42 +0100

    audit module to jfa 0.5.0 (#18)

--- tests/testthat/test-auditbayesianplanning.R
+++ tests/testthat/test-auditbayesianplanning.R
@@ -181,12 +181,12 @@ test_that("Across Expected Errors (Current: 0) plot matches 3", {
 test_that("<b>Table 3.</b> Descriptive Statistics for Prior and Expected Posterior Distribution results match", {
 	table <- results[["results"]][["planningContainer"]][["collection"]][["planningContainer_priorAndPosteriorStatistics"]][["data"]]
 	jaspTools::expect_equal_tables(table,
-		list(9488, "beta-binomial(N = 9987, <unicode><unicode> = 1, <unicode><unicode> = 1)",
-			 0.20034040849018, 0.79965959150982, "NaN", 0.250532114686352,
-			 "NaN", "Prior", 1925, "beta-binomial(N = 9987, <unicode><unicode> = 1, <unicode><unicode> = 14)",
-			 0.95618100435991, 0.0438189956400898, 0, 21.8211529130783, 0.1925,
-			 "Expected posterior", -0.7563, "", 4.7727815450011, 0.0547970612812336,
-			 "NaN", 87.0992245461097, "", "Expected shift"))
+		list(9500, "beta-binomial(N = 9987, <unicode><unicode> = 1, <unicode><unicode> = 1)",
+			 0.200079992000814, 0.799920007999186, 523, 0.250125000000022,
+			 0.8977, "Prior", 1927, "beta-binomial(N = 9987, <unicode><unicode> = 1, <unicode><unicode> = 14)",
+			 0.955981093321683, 0.0440189066783169, 0, 21.7175110755894,
+			 0.1927, "Expected posterior", -7573, "", 4.77799445992478, 0.0550291357112318,
+			 -523, 86.8266309868567, "", "Expected shift"))
 })
 
 test_that("Implied Prior Distribution plot matches 3", {
@@ -204,5 +204,5 @@ test_that("<b>Table 2.</b> Implicit Sample results match", {
 test_that("<b>Table 1.</b> Planning Summary results match", {
 	table <- results[["results"]][["planningContainer"]][["collection"]][["planningContainer_summaryTable"]][["data"]]
 	jaspTools::expect_equal_tables(table,
-		list("5%", 87.0992245461097, 21.8211529130783, 0, "$ 2000", 13))
+		list("5%", 86.8266309868567, 21.7175110755894, 0, "$ 2000", 13))
 })
