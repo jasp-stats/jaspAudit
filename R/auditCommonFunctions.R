c624221b084d6d53455aa9b7545dd1201970c2f3
Koen Derks <koen-derks@hotmail.com>
2023-04-21 15:14:39 +0200

    Translation fixes (#319)
    * Add missing gettexts
    
    * Update auditClassicalBenfordsLaw.R
    
    * Another missing gettext

--- R/auditCommonFunctions.R
+++ R/auditCommonFunctions.R
@@ -1803,15 +1803,15 @@ gettextf <- function(fmt, ..., domain = NULL) {
 
     if (options[["bayesian"]]) {
       dist <- switch(options[["likelihood"]],
-        "binomial" = "Beta",
-        "poisson" = "Gamma",
-        "hypergeometric" = "Beta-binomial"
+        "binomial" = gettext("Beta"),
+        "poisson" = gettext("Gamma"),
+        "hypergeometric" = gettext("Beta-binomial")
       )
     } else {
       dist <- switch(options[["likelihood"]],
-        "binomial" = "Binomial",
-        "poisson" = "Poisson",
-        "hypergeometric" = "Hypergeometric"
+        "binomial" = gettext("Binomial"),
+        "poisson" = gettext("Poisson"),
+        "hypergeometric" = gettext("Hypergeometric")
       )
     }
 
@@ -1833,7 +1833,7 @@ gettextf <- function(fmt, ..., domain = NULL) {
       leftPlotError <- try({
         for (i in seq_along(likelihoods)) {
           if (options[["bayesian"]]) {
-            names <- if (parentState[["N.units"]] > 0) c("Gamma", "Beta", "Beta-binomial") else c("Gamma", "Beta")
+            names <- if (parentState[["N.units"]] > 0) c(gettext("Gamma"), gettext("Beta"), gettext("Beta-binomial")) else c(gettext("Gamma"), gettext("Beta"))
             risks <- .jfaAuditRiskModelCalculation(options)
             # Create a prior distribution that incorporates the existing information
             prior <- jfa::auditPrior(
@@ -1843,7 +1843,7 @@ gettextf <- function(fmt, ..., domain = NULL) {
               n = options[["n_prior"]], x = options[["x_prior"]], alpha = options[["alpha"]], beta = options[["beta"]]
             )
           } else {
-            names <- if (parentState[["N.units"]] > 0) c("Poisson", "Binomial", "Hypergeometric") else c("Poisson", "Binomial")
+            names <- if (parentState[["N.units"]] > 0) c(gettext("Poisson"), gettext("Binomial"), gettext("Hypergeometric")) else c(gettext("Poisson"), gettext("Binomial"))
             prior <- FALSE
           }
 
@@ -2532,13 +2532,14 @@ gettextf <- function(fmt, ..., domain = NULL) {
       sampleSizeMessage <- planningState[["n"]]
     }
 
+    label_combine <- if (options[["bayesian"]]) gettext("combined with the information in the prior distribution ") else ""
     message <- gettextf(
       "The purpose of the evaluation stage is to infer the misstatement \u03B8 in the population on the basis of a sample.\n\nThe population consisted of %1$s items and %2$s units. The sample consisted of %3$s sampling units, of which a total of %4$s were misstated. The information from this sample %5$s results in a most likely misstatement in the population of %6$s and a %7$s upper bound of %8$s.",
       if (planningOptions[["N.items"]][1] == 0) "..." else format(sum(planningOptions[["N.items"]]), scientific = FALSE),
       if (planningOptions[["N.units"]][1] == 0) "..." else format(sum(planningOptions[["N.units"]]), scientific = FALSE),
       sampleSizeMessage,
       errors,
-      if (options[["bayesian"]]) gettext("combined with the information in the prior distribution ") else "",
+      label_combine,
       mleLabel,
       planningOptions[["conf_level_label"]],
       boundLabel
