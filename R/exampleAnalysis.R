328efc7f2672f8553b0a9049951b8b921fadae73
federicapicogna <128388440+federicapicogna@users.noreply.github.com>
2024-09-23 09:52:09 +0200

    Update exampleAnalysis.R

--- R/exampleAnalysis.R
+++ R/exampleAnalysis.R
@@ -89,7 +89,8 @@ exampleAnalysis <- function(jaspResults, dataset, options, ...) {
     TP indicates the number of True Positives, meaning the number of items with a true positive classification that the AI also classifies as positive, and FP indicates the number of False Positives, meaning the number of items with a true negative classification that the AI classifies as positive. N indicates the total number of items classified by AI. Then, the Positive Rate of the privileged group is divided by the Positive Rate of the unprivileged group (PP_privileged/PP_unprivileged). After calculating this ratio, Disparate Impact is obtained.
     
     Further Details:
-    DETAILS")
+    The most common use of Disparate Impact is tied to the U.S. Equal Employment Opportunity Commission (EEOC) guidelines. Specifically, the EEOC established the 80% rule. Based on this rule, unlike other fairness measures, this one uses a fixed threshold, equal to 0.80, that is neither 0 nor 1 and does not involve statistical testing to assess AI fairness. Instead, the auditor directly compares the ratio of the positive rates between the two groups to the threshold value, where the positive rate is the model evaluation metric on which Disparate Impact is based. In fact, when the value of the Disparate Impact is higher than 0.80, the use of AI is fair. 
+    This deterministic approach is chosen because the measure is applied across the entire dataset, which usually has a large enough size that makes statistical significance less of a concern. However, this method has its limitations. The threshold value is not grounded in the properties of the measure, the nature of the data, or any other statistical considerations. The rule was originally based on the hiring rates in the U.S. employment context, which lacks theoretical rigor for broader applications. Additionally, when the ratio is close to the threshold, such as 0.79 or 0.81, drawing definitive conclusions about discrimination becomes uncertain.")
   }
   else {
      if(options[["secondquestion"]]=="secondquestion_both"){
@@ -103,7 +104,7 @@ exampleAnalysis <- function(jaspResults, dataset, options, ...) {
       TP indicates the number of True Positives, meaning the number of items with a true positive classification that the AI also classifies as positive, TN indicates the number of True Negatives, meaning the number of items with a true negative classification that the AI also classifies as negative, and FP indicates the number of False Positives, meaning the number of items with a true negative classification that the AI classifies as positive.
       
       Further Details:
-      DETAILS")
+      It is important to note that this is the only measure that focuses on both the correct classification of the positive class and the incorrect classification of the negative class. Its formula considers how many observations known to be true positives were correctly identified and classified by the AI, and among all observations known to be true negatives, how many were incorrectly identified and classified by the AI. For this reason, it is completely different from all other fairness measures.")
      }
 
      else {
@@ -117,10 +118,7 @@ exampleAnalysis <- function(jaspResults, dataset, options, ...) {
               
               Fairness Measure Formula:
               The Predictive Rate Parity is based on the Precision. Therefore, the Precision, whose formula is TP/(TP+FP), is applied to both the privileged group and the unprivileged group. 
-              TP indicates the number of True Positives, meaning the number of items with a true positive classification that AI also classifies as positive, and FP indicates the number of False Positives, meaning the number of items with a true negative classification that AI classifies as positive.
-              
-              Further Details:
-              DETAILS")
+              TP indicates the number of True Positives, meaning the number of items with a true positive classification that AI also classifies as positive, and FP indicates the number of False Positives, meaning the number of items with a true negative classification that AI classifies as positive.")
             }
             else {
                ExplanationText <- gettext("Fairness Definition: 
@@ -130,10 +128,7 @@ exampleAnalysis <- function(jaspResults, dataset, options, ...) {
                
                Fairness Measure Formula:
                Equal Opportunity is based on the True Positive Rate. Therefore, the True Positive Rate, whose formula is TP/(TP+FN), is applied to both the privileged group and the unprivileged group. 
-               TP indicates the number of True Positives, meaning the number of items with a true positive classification that AI also classifies as positive, and FN indicates the number of False Negatives, meaning the number of items with a true positive classification that AI classifies as negative.
-               
-               Further Details:
-               DETAILS")
+               TP indicates the number of True Positives, meaning the number of items with a true positive classification that AI also classifies as positive, and FN indicates the number of False Negatives, meaning the number of items with a true positive classification that AI classifies as negative.")
             }
           }
           else {
@@ -145,10 +140,7 @@ exampleAnalysis <- function(jaspResults, dataset, options, ...) {
                 
                 Fairness Measure Formula:
                 The Specificity Parity is based on the True Negative Rate. Therefore, the True Negative Rate, whose formula is TN/(TN+FP), is applied to both the privileged group and the unprivileged group. 
-                FP indicates the number of False Positives, meaning the number of items with a true negative classification that AI classifies as positive, and TN indicates the number of True Negatives, meaning the number of items with a true negative classification that AI also classifies as negative.
-                
-                Further Details:
-                DETAILS")
+                FP indicates the number of False Positives, meaning the number of items with a true negative classification that AI classifies as positive, and TN indicates the number of True Negatives, meaning the number of items with a true negative classification that AI also classifies as negative.")
              }
              else {
                 if (options[["fourthquestion_caseB"]]=="fourthquestion_caseB_FN") {
@@ -159,10 +151,7 @@ exampleAnalysis <- function(jaspResults, dataset, options, ...) {
                    
                    Fairness Measure Formula:
                    The Negative Predictive Rate Parity is based on the Negative Predictive Rate. Therefore, the Negative Predictive Rate, whose formula is TN/(TN+FN), is applied to both the privileged group and the unprivileged group. 
-                   FN indicates the number of False Negatives, meaning the number of items with a true positive classification that the AI classifies as negative, and TN indicates the number of True Negatives, meaning the number of items with a true negative classification that AI also classifies as negative.
-                   
-                   Further Details:
-                   DETAILS")
+                   FN indicates the number of False Negatives, meaning the number of items with a true positive classification that the AI classifies as negative, and TN indicates the number of True Negatives, meaning the number of items with a true negative classification that AI also classifies as negative.")
                 }
                 else {
                     ExplanationText <- gettext("Fairness Definition: 
@@ -175,7 +164,8 @@ exampleAnalysis <- function(jaspResults, dataset, options, ...) {
                     TP indicates the number of True Positives, meaning the number of items with a true positive classification that the AI also classifies as positive, and TN indicates the number of True Negatives, meaning the number of items with a true negative classification that the AI also classifies as negative. The sum of TP and TN is therefore the total number of correct classifications made by the AI. N indicates the total number of items classified by AI.
                     
                     Further Details:
-                    DETAILS")
+                    Accuracy Parity has, just like Accuracy as a model evaluation metric, a sever deficit when applied to cases were the positive class consists of just a small minority. Consider a scenario where 3% of the privileged group (90% of the population) commits fraud, compared to 6% of the unprivileged group (10% of the population). Without applying a fraud detection AI, the overall accuracy seems high at 96.7%, but this masks a disparity: accuracy for the unprivileged group is lower at 94%, while for the privileged group it is 97%.
+                    It should be noted that, unlike all other measures, Accuracy Parity does not account for varying error costs related to misclassifications. If there is an increase in false negatives, false positives, or both, and there are associated costs that need to be minimized, Accuracy Parity will fail to detect these increases in errors, and as a result, it will not capture the associated rise in costs.")
                 }
              }
           }
@@ -189,10 +179,7 @@ exampleAnalysis <- function(jaspResults, dataset, options, ...) {
               
               Fairness Measure Formula:
               The False Positive Rate Parity is based on the False Positive Rate. Therefore, the False Positive Rate, whose formula is FP/(TN+FP), is applied to both the privileged group and the unprivileged group. 
-              FP indicates the number of False Positives, meaning the number of items with a true negative classification that AI classifies as positive, and TN indicates the number of True Negatives, meaning the number of items with a true negative classification that AI also classifies as negative. 
-              
-              Further Details:
-              DETAILS")
+              FP indicates the number of False Positives, meaning the number of items with a true negative classification that AI classifies as positive, and TN indicates the number of True Negatives, meaning the number of items with a true negative classification that AI also classifies as negative. ")
            }
            else {
               ExplanationText <- gettext("Fairness Definition: 
@@ -202,10 +189,7 @@ exampleAnalysis <- function(jaspResults, dataset, options, ...) {
               
               Fairness Measure Formula:
              The False Negative Rate Parity is based on the False Negative Rate. Therefore, the False Negative Rate, whose formula is FN/(TP+FN), is applied to both the privileged group and the unprivileged group. 
-             FN indicates the number of False Negatives, meaning the number of items with a true positive classification that the AI classifies as negative, and TP indicates the number of True Positives, meaning the number of items with a true positive classification that the AI also classifies as positive.
-             
-             Further Details:
-             DETAILS")
+             FN indicates the number of False Negatives, meaning the number of items with a true positive classification that the AI classifies as negative, and TP indicates the number of True Positives, meaning the number of items with a true positive classification that the AI also classifies as positive.")
            }
         }
      }
